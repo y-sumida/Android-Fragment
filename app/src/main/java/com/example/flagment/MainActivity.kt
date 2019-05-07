56543c85444a33a834c5589e6b83a049821275ea
Yuki Sumida <y.sumida.0201@gmail.com>
2019-05-07 23:55:53 +0900

    ボタンフラグメントを配置する

--- app/src/main/java/com/example/flagment/MainActivity.kt
+++ app/src/main/java/com/example/flagment/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.flagment
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ButtonFragment.OnButtonClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onButtonClicked() {
+        print("button cliked")
     }
 }
