6787ebf527a2f7db927f135910a1379f8f00a8be
Yuki Sumida <y.sumida.0201@gmail.com>
2019-05-08 00:02:31 +0900

    ラベルフラグメントを動的に配置する

--- app/src/main/java/com/example/flagment/LabelFragment.kt
+++ app/src/main/java/com/example/flagment/LabelFragment.kt
@@ -22,7 +22,8 @@ class LabelFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(R.layout.label_fragment, container, false)
-        view.textView.text = counter.toString()
+        counterLabel = view.textView
+        counterLabel.text = counter.toString()
         return view
     }
 
--- app/src/main/java/com/example/flagment/MainActivity.kt
+++ app/src/main/java/com/example/flagment/MainActivity.kt
@@ -8,9 +8,16 @@ class MainActivity : AppCompatActivity(), ButtonFragment.OnButtonClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (supportFragmentManager.findFragmentByTag("labelFragment") == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container, newLabelFragment(0), "labelFragment")
+                .commit()
+        }
     }
 
     override fun onButtonClicked() {
-        print("button cliked")
+        val fragment = supportFragmentManager.findFragmentByTag("labelFragment") as LabelFragment
+        fragment.update()
     }
 }
